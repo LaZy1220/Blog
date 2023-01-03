a91195aeafa0fe51adc07ba955bec05e2cfc45ab
LaZy1220 <4elove4iska@gmail.com>
2023-01-03 19:34:36 +0300

    registration finish

--- server.js
+++ server.js
@@ -21,23 +21,43 @@ app.get("/", (req, res) => {
 });
 
 app.post("/auth/register", registerValidator, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+    //hashpass
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+    //creatuser
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      passwordHash: hash,
+      avatarUrl: req.body.avatarUrl,
+    });
+    const user = await doc.save();
+    //create token
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret",
+      { expiresIn: "30d" }
+    );
+    //response user(not pass) and token
+    const { passwordHash, ...userData } = user._doc;
+    res.json({
+      ...userData,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось зарегестрироваться",
+    });
   }
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    passwordHash,
-    avatarUrl: req.body.avatarUrl,
-  });
-  const user = await doc.save();
-
-  res.json(user);
 });
 app.listen(4000, (error) => {
   error ? console.log(error) : console.log("Server listening 4000....");
