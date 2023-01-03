a62ddc24d3f58f9762898f3def174e26fd864f29
LaZy1220 <4elove4iska@gmail.com>
2023-01-03 19:09:02 +0300

    add crypt

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 import { validationResult } from "express-validator";
+import UserModel from "./models/User.js";
 import { registerValidator } from "./validations/auth.js";
 
 mongoose.set("strictQuery", false);
@@ -18,14 +20,24 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.post("/auth/register", registerValidator, (req, res) => {
+app.post("/auth/register", registerValidator, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
-  res.json({
-    success: true,
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    passwordHash,
+    avatarUrl: req.body.avatarUrl,
   });
+  const user = await doc.save();
+
+  res.json(user);
 });
 app.listen(4000, (error) => {
   error ? console.log(error) : console.log("Server listening 4000....");
