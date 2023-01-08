141acbcf53a3643a31ff84456a48a19d37e4293b
LaZy1220 <4elove4iska@gmail.com>
2023-01-08 19:02:25 +0300

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Header } from "./components/Header";
+import { Header } from "./components/Header/Header";
 import { HomePage } from "./pages/HomePage";
 import { FullPostPage } from "./pages/FullPostPage";
 import { AddPostsPage } from "./pages/AddPostsPage";
