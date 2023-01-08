32ac67d3318e15fdb579a93b28c8e7b0e4bb5df1
LaZy1220 <4elove4iska@gmail.com>
2023-01-08 18:43:53 +0300

    add routs

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { HomePage } from "./pages/HomePage";
+import { FullPostPage } from "./pages/FullPostPage";
+import { AddPostsPage } from "./pages/AddPostsPage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 
 function App() {
-  return <div>Test</div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/posts/:id" element={<FullPostPage />} />
+        <Route path="/add-post" element={<AddPostsPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
