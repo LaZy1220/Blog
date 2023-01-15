9e3d51fde84f5f54cbcec837fb084f1164af33a5
LaZy1220 <4elove4iska@gmail.com>
2023-01-15 14:40:15 +0300

    upload image on server

--- client/src/pages/AddPost/index.jsx
+++ client/src/pages/AddPost/index.jsx
@@ -3,13 +3,35 @@ import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import SimpleMDE from "react-simplemde-editor";
-
+import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectIsAuth } from "../../redux/slices/auth";
 import "easymde/dist/easymde.min.css";
 import styles from "./AddPost.module.scss";
+import { useState, useRef } from "react";
+import axios from "../../axios";
 
 export const AddPost = () => {
-  const [value, setValue] = React.useState("");
+  const isAuth = useSelector(selectIsAuth);
+  const [value, setValue] = useState("");
+  const [title, setTitle] = useState("");
+  const [tags, setTags] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const inputFileRef = useRef(null);
 
+  const handleChangeFile = async (e) => {
+    try {
+      const formData = new FormData();
+      const file = e.target.files[0];
+      formData.append("image", file);
+      const { data } = await axios.post("/upload", formData);
+      setImageUrl(data.url);
+    } catch (err) {
+      console.warn(err);
+      alert("Ошибка загрузки файла!!!");
+    }
+  };
+  const onClickRemoveImage = () => {};
   const onChange = React.useCallback((value) => {
     setValue(value);
   }, []);
@@ -28,23 +50,53 @@ export const AddPost = () => {
     }),
     []
   );
-
+  if (!window.localStorage.getItem("token") && !isAuth) {
+    return <Navigate to="/" />;
+  }
   return (
     <Paper style={{ padding: 30 }}>
-      <Button variant="outlined" size="large">
+      <Button
+        onClick={() => inputFileRef.current.click()}
+        variant="outlined"
+        size="large"
+      >
         Загрузить превью
       </Button>
-      <br />
-      <br />
+      <input
+        ref={inputFileRef}
+        type="file"
+        onChange={handleChangeFile}
+        hidden
+      />
+      {imageUrl && (
+        <>
+          <Button
+            variant="conteined"
+            color="error"
+            onClick={onClickRemoveImage}
+          >
+            Удалить
+          </Button>
+          <img
+            className={styles.image}
+            src={`http://localhost:4000${imageUrl}`}
+            alt="Uploaded"
+          />
+        </>
+      )}
       <TextField
         classes={{ root: styles.title }}
         variant="standard"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
         placeholder="Заголовок статьи..."
         fullWidth
       />
       <TextField
         classes={{ root: styles.tags }}
         variant="standard"
+        value={tags}
+        onChange={(e) => setTags(e.target.value)}
         placeholder="Тэги"
         fullWidth
       />
